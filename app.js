a1513fa98e1efd8bab9a5fd6c8e34bc5f57bacd8
Markus Rauhut <markus.rauhut@gmail.com>
2023-06-06 12:01:37 +0200

    Add game functionality

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import WebSocket, { WebSocketServer } from "ws";
 import { nanoid } from "nanoid";
+import { getRandomWords } from "./word-list.js";
 
 const port = 3333;
 
@@ -7,21 +8,80 @@ const wss = new WebSocketServer({ port });
 
 console.log("skribekk server startet on port", port);
 
+let hostId, word;
+let gameStarted = false;
+
 wss.on("connection", function connection(ws) {
-  ws.id = nanoid();
+  const clientId = nanoid(16);
+  let clientName = "";
+
+  const sendToAllClients = (msg) => {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(msg, {
+          binary: true,
+        });
+      }
+    });
+  };
+
+  const sendToOtherClients = (msg) => {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(msg, {
+          binary: true,
+        });
+      }
+    });
+  };
 
   ws.on("error", console.error);
 
-  ws.on("message", function message(data) {
-    console.log("received: %s", data);
+  ws.on("message", function message(msg) {
+    const { type, data } = JSON.parse(msg);
 
-    wss.clients.forEach(function each(client) {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-
-    // Answer to specific client
-    // ws.send(data)
+    switch (type) {
+      case "setName":
+        console.log(`${data} joined the game! (clientID: ${clientId})`);
+        clientName = data;
+        sendToOtherClients(msg);
+        break;
+      case "startGame":
+        console.log("Game started by", clientName);
+        hostId = clientId;
+        gameStarted = true;
+        // fetch("https://random-word-form.herokuapp.com/random/animal/a?count=3")
+        //   .then((r) => r.json())
+        //   .then((d) => console.log({ type, data: JSON.stringify(d) }));
+        ws.send(JSON.stringify({ type, data: getRandomWords(3) }), {
+          binary: true,
+        });
+        break;
+      case "setWord":
+        console.log("Word to guess:", data);
+        word = data;
+        sendToOtherClients(JSON.stringify({ type: "gameStarted", data: null }));
+        break;
+      case "draw":
+        if (gameStarted && clientId === hostId) {
+          sendToAllClients(msg);
+        }
+        if (!gameStarted) {
+          sendToAllClients(msg);
+        }
+        break;
+      case "guess":
+        console.log(`${clientName} guessed: ${data}`);
+        if (data.toLowerCase() === word.toLowerCase()) {
+          console.log("=> guessed correctly!");
+          sendToAllClients(
+            JSON.stringify({
+              type: "wordGuessed",
+              data: { name: clientName, word },
+            })
+          );
+        }
+        break;
+    }
   });
 });
